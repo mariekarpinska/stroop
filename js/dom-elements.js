e128f3bd4cc20639ffa5f3c4482c397ae66e126f
mariekarpinska <karpinska@umail.ucsb.edu>
2023-12-06 19:31:47 -0800

    language

--- js/dom-elements.js
+++ js/dom-elements.js
@@ -1,36 +1,38 @@
 export const domElements = {
-    buttons: {
-        start: document.getElementById('start-button'),
-        redChoice: document.getElementById('red-choice-button'),
-        greenChoice: document.getElementById('green-choice-button'),
-        blueChoice: document.getElementById('blue-choice-button'),
-        download: document.getElementById('download-button'),
-    },
+  buttons: {
+    start: document.getElementById("start-button"),
+    redChoice: document.getElementById("red-choice-button"),
+    greenChoice: document.getElementById("green-choice-button"),
+    blueChoice: document.getElementById("blue-choice-button"),
+    download: document.getElementById("download-button"),
+  },
 
-    timeDivs: {
-        matched: document.getElementById('matched-time-div'),
-        mismatched: document.getElementById('mismatched-time-div'),
-    },
+  timeDivs: {
+    matched: document.getElementById("matched-time-div"),
+    mismatched: document.getElementById("mismatched-time-div"),
+  },
 
-    containers: {
-        starting: document.getElementById('start-container'),
-        test: document.getElementById('test-container'),
-        result: document.getElementById('result-container'),
-        error: document.getElementById('error-container'),
-    },
-    
-    wordDisplayArea: document.getElementById('word-display-area'),
-    incorrectDiv: document.getElementById('incorrect-div'),
-    errorMessage: document.getElementById('error-message'),
-    errorSound: document.getElementById('error-sound'),
+  containers: {
+    starting: document.getElementById("start-container"),
+    test: document.getElementById("test-container"),
+    result: document.getElementById("result-container"),
+    error: document.getElementById("error-container"),
+  },
 
-    form: document.getElementById('form'),
+  wordDisplayArea: document.getElementById("word-display-area"),
+  incorrectDiv: document.getElementById("incorrect-div"),
+  errorMessage: document.getElementById("error-message"),
+  errorSound: document.getElementById("error-sound"),
 
-    formElements: {
-        stimulusNumberField: document.getElementById('stimulus-number'),
-        questionIcon: document.getElementById('question-icon'),
-        quantityInformation: document.getElementById('quantity-information'),
-        wordMeaningRadioButton: document.getElementById('word-meaning'),
-        wordColorRadioButton: document.getElementById('word-color'),
-    },
+  form: document.getElementById("form"),
+
+  formElements: {
+    stimulusNumberField: document.getElementById("stimulus-number"),
+    questionIcon: document.getElementById("question-icon"),
+    quantityInformation: document.getElementById("quantity-information"),
+    wordMeaningRadioButton: document.getElementById("word-meaning"),
+    wordColorRadioButton: document.getElementById("word-color"),
+    englishRadioButton: document.getElementById("english"),
+    espanolRadioButton: document.getElementById("espanol"),
+  },
 };
